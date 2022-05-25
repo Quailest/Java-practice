84094ec99f4e9c2294348750d70e75a98ae1c6d1
ちょら <rkk311524@gmail.com>
2022-05-25 19:56:00 +0900

    [finish]chap7

---
+++ src/chapter07.java
@@ -0,0 +1,23 @@
+
+public class chapter07 {
+	public static void main(String[] args) {
+		int distance = 12;
+		
+		if(distance <= 12) {
+			System.out.println("とても近いです");
+			
+		}
+		else if (distance <= 10) {
+			System.out.println("近いです");
+			
+		}
+		else if (distance <= 15) {
+			System.out.println("遠いです");
+			
+		}
+		else {
+			System.out.println("とても遠いです");
+			
+		}
+	}
+}
