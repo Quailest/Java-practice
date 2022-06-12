116bda35c4b7e3d23a25b572951a94ac8e2a3a13
ちょら <rkk311524@gmail.com>
2022-06-12 19:55:37 +0900

    addchap13

---
+++ src/polymorphism/human/Human.java
@@ -0,0 +1,12 @@
+package polymorphism.human;
+
+public class Human {
+	private String name;
+    protected int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
