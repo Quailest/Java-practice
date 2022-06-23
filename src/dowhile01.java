0de787614a6b3666218784c9b0c271f7df72eea5
ちょら <rkk311524@gmail.com>
2022-06-23 22:46:49 +0900

    comit

---
+++ src/dowhile01.java
@@ -0,0 +1,11 @@
+
+public class dowhile01 {
+	public static void main(String[] args) {
+		int number = 1;
+		do {
+			number *= 2;
+			System.out.println("sowhile01 = "+ number);
+			
+		}while (number < 50);
+	}
+}
